86c20539ffea7e3e90d2169b3c6572ff39628c04
zts123 <43432280+zts123@users.noreply.github.com>
2022-04-27 23:26:50 +0800

    update

--- src/main/java/com/zts/git/GitTest.java
+++ src/main/java/com/zts/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-fix git");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("github项目更新");
     }
 }
